cdca2508eb137d30c865bf26b63dbe20f23b6138
RKarb <36237554+RKarb@users.noreply.github.com>
2021-06-07 18:08:27 +0100

    Blu Model + Movement
    A model of Blu has been created as the player avatar complete with basic animations. The player is able to move with the camera following behind them.

---
+++ Assets/Scripts/Blu/CharControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+
+    public GameObject thePlayer;
+    public bool isWalking;
+    public float horizontalMove;
+    public float verticalMove;
+
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") || (Input.GetButton("Vertical")))
+        {
+            thePlayer.GetComponent<Animation>().Play("cat_armature_walk");
+            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 8;
+            isWalking = true;
+            transform.Rotate(0, horizontalMove, 0);
+            transform.Translate(0, 0, verticalMove);
+        }
+        else
+        {
+            thePlayer.GetComponent<Animation>().Play("cat_armature_idle");
+            isWalking = false;
+        }
+    }
+}
